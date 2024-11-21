bfe29b71d2fa43750a1341d5f69b41da9d6b5159
Vat-9 <vatvar73@gmail.com>
2024-11-21 13:13:14 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "58xFlQKK#SsgGzEMkINqPtc8V9SmGAZU5CEam-2w4tjv1xUQV0SY",
 };
